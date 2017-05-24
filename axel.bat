bc64c9d02b1ad3f2fe84f176939ad307441c59e3
U-LOGON\212593872 <212593872@G1ZBDRF2E.logon.ds.ge.com>
2017-05-25 07:17:24 +0800

    remove err log

--- axel.bat
+++ axel.bat
@@ -5,7 +5,6 @@ set PORXY=http://3.20.128.6:88
 set BAT_HOME=%~dp0
 set AXEL_EXE=%BAT_HOME%\axel.exe
 set AXEL_LOG=%BAT_HOME%\axel.log
-set AXEL_ERR=%BAT_HOME%\axel.err.log
 set PASTE_EXE=%BAT_HOME%\paste.exe
 set DOWNLOAD_HOME=%USERPROFILE%\Downloads
 set SHORT_CUT=%USERPROFILE%\Desktop\Axel.lnk
