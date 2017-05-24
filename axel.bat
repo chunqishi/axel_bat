6caef8f01143a512ec5f51812c1e9c0344c8c6d3
Shi <212593872@logon.ds.ge.com>
2017-05-24 14:18:58 +0100

    added no range model

--- axel.bat
+++ axel.bat
@@ -1,12 +1,10 @@
 @echo off
 
-
-
-set http_proxy=http://165.225.96.34:10015
-set https_proxy=http://165.225.96.34:10015
+set PORXY=http://3.20.128.6:88
 
 set BAT_HOME=%~dp0
 set AXEL_EXE=%BAT_HOME%\axel.exe
+set AXEL_LOG=%BAT_HOME%\axel.log
 set PASTE_EXE=%BAT_HOME%\paste.exe
 set DOWNLOAD_HOME=%USERPROFILE%\Downloads
 set SHORT_CUT=%USERPROFILE%\Desktop\Axel.lnk
@@ -14,9 +12,20 @@ If Not Exist %SHORT_CUT% %BAT_HOME%\XXMKLINK.EXE %USERPROFILE%\Desktop\Axel %BAT
 
 CD %DOWNLOAD_HOME%
 
+call %BAT_HOME%curl.exe -s --connect-timeout 1 --proxy %PORXY% www.baidu.com
+If "%errorlevel%"=="0"  (
+	set http_proxy=%PORXY%
+	set https_proxy=%PORXY%
+)
+
+
 for /f "tokens=*" %%a in ('%PASTE_EXE%') do (
   echo Downloading %%a ...
+  echo Downloading %%a >> %AXEL_LOG%
   %AXEL_EXE% -n 20 "%%a"
+  If Not "%errorlevel%"=="0" (
+	%BAT_HOME%curl.exe -L -O "%%a"
+  )  
 )
 
 start %DOWNLOAD_HOME%
--- axel_urls.bat
+++ axel_urls.bat
@@ -1,9 +1,37 @@
-set http_proxy=http://165.225.96.34:10015
-set https_proxy=http://165.225.96.34:10015
+rem set http_proxy=http://165.225.96.34:10015
+rem set https_proxy=http://165.225.96.34:10015
+
+
+set URLS=urls.txt
+
+set PORXY=http://3.20.128.6:88
+
+
+set BAT_HOME=%~dp0
+set AXEL_EXE=%BAT_HOME%\axel.exe
+set AXEL_LOG=%BAT_HOME%\axel.log
+set PASTE_EXE=%BAT_HOME%\paste.exe
+set DOWNLOAD_HOME=%USERPROFILE%\Downloads
+
+CD %DOWNLOAD_HOME%
+
+call %BAT_HOME%curl.exe -s --connect-timeout 1 --proxy %PORXY% www.baidu.com
+if "%errorlevel%"=="0"  (
+	set http_proxy=%PORXY%
+	set https_proxy=%PORXY%
+)
+
 
 @echo off
-for /f "tokens=*" %%a in (urls.txt) do (
-  echo downloading %%a
-  axel -n 20 "%%a"
+for /f "tokens=*" %%a in (%BAT_HOME%%URLS%) do (
+  echo Downloading %%a ...
+  echo Downloading %%a >> %AXEL_LOG%
+  %AXEL_EXE% -n 20 "%%a"
+  If Not "%errorlevel%"=="0" (
+	%BAT_HOME%curl.exe -L -O "%%a"
+  )  
 )
+
+
+start %DOWNLOAD_HOME%
 pause
