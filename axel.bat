e5001c070688df3e70c9ca5ddc90ac60258ff5ba
U-LOGON\212593872 <212593872@G1ZBDRF2E.logon.ds.ge.com>
2017-05-25 06:43:48 +0800

    fix error download

--- axel.bat
+++ axel.bat
@@ -5,6 +5,7 @@ set PORXY=http://3.20.128.6:88
 set BAT_HOME=%~dp0
 set AXEL_EXE=%BAT_HOME%\axel.exe
 set AXEL_LOG=%BAT_HOME%\axel.log
+set AXEL_ERR=%BAT_HOME%\axel.err.log
 set PASTE_EXE=%BAT_HOME%\paste.exe
 set DOWNLOAD_HOME=%USERPROFILE%\Downloads
 set SHORT_CUT=%USERPROFILE%\Desktop\Axel.lnk
@@ -12,7 +13,7 @@ If Not Exist %SHORT_CUT% %BAT_HOME%\XXMKLINK.EXE %USERPROFILE%\Desktop\Axel %BAT
 
 CD %DOWNLOAD_HOME%
 
-call %BAT_HOME%curl.exe -s --connect-timeout 1 --proxy %PORXY% www.baidu.com
+call %BAT_HOME%curl.exe -s --connect-timeout 1 --proxy %PORXY% www.baidu.com  > NUL
 If "%errorlevel%"=="0"  (
 	set http_proxy=%PORXY%
 	set https_proxy=%PORXY%
@@ -22,9 +23,8 @@ If "%errorlevel%"=="0"  (
 for /f "tokens=*" %%a in ('%PASTE_EXE%') do (
   echo Downloading %%a ...
   echo Downloading %%a >> %AXEL_LOG%
-  %AXEL_EXE% -n 20 "%%a"
-  If Not "%errorlevel%"=="0" (
-	%BAT_HOME%curl.exe -L -O "%%a"
+  %AXEL_EXE% -n 20 %%a  || (
+	%BAT_HOME%curl.exe -L -O -k %%~a
   )  
 )
 
